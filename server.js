12a1eba2c855405d1cc48522ad18acae4ac3d875
TonyaTer <65990995+TonyaTer@users.noreply.github.com>
2020-10-14 17:59:36 -0400

    uploaded files

---
+++ server.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+const express = require('express');
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
+const signin = require('./controllers/signin');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : process.env.DB_HOST,
+      user :  process.env.DB_USER,
+      password :  process.env.DB_PASSWORD,
+      database :  process.env.DB_NAME
+    }
+});
+
+
+const app = express();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get('/profile/:id', profile.handleProfile(db));
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', register.handleRegister(db, bcrypt));
+app.put('/image', image.handleImage(db));
+app.post('/imageurl', image.handleAPIcall());
+
+
+app.listen(3000, ()=>{
+    console.log(`Listening on port 3000`);
+});
